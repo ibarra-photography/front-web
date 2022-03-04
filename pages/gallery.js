e74122f36e2aab249f9b38f0f93402a0095d3f26
Ibarra <aitoribarra@tutanota.com>
2022-03-04 13:14:28 +0100

    ADD - fetched imgs gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 import Navigation from "../components/layout/Navigation/Navigation";
 import PhotoCard from "../components/PhotoCard/PhotoCard";
+import PhotoCardsGrid from "../components/PhotoCardsGrid/PhotoCardsGrid";
 
 import styles from "./../styles/gallery.module.css";
 
 const gallery = () => {
   return (
-    <Fragment>
+    <div className={styles["gallery-container"]}>
       <Navigation />
       <h1>Gallery</h1>
-      <div className={styles["photo-grid-container"]}></div>
-    </Fragment>
+      <div className={styles["photo-grid-container"]}>
+        <PhotoCardsGrid />
+      </div>
+    </div>
   );
 };
 
