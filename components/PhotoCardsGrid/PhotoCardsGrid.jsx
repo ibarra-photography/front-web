56f16b39a385b29974cdb1c82fb6162484f7babe
Ibarra <aitoribarra@tutanota.com>
2022-03-10 11:33:26 +0100

    ADD - Base 64 to Blob Converter

--- components/PhotoCardsGrid/PhotoCardsGrid.jsx
+++ components/PhotoCardsGrid/PhotoCardsGrid.jsx
@@ -4,22 +4,24 @@ import PhotoCard from "../PhotoCard/PhotoCard";
 
 import fetchApiData from "../../services/fetchApiData";
 
+import b64toBlob from "../../services/base64toBlob.js";
+
 const PhotoCardsGrid = () => {
   const [photos, setPhotos] = useState(null);
   useEffect(() => {
     fetchApiData().then((response) => setPhotos(response));
   }, []);
+  if (photos) {
+    console.log(photos);
+  }
+
   return (
     <Fragment>
       {photos?.map((photo) => {
-        console.log("in");
+        const blob = b64toBlob(photo.data);
+        const blobUrl = URL.createObjectURL(blob);
         return (
-          <PhotoCard
-            key={photo._id}
-            description=""
-            link={`http://localhost:4000/api/v1/photos/${photo._id}.jpg`}
-            title=""
-          />
+          <PhotoCard key={photo._id} description="" link={blobUrl} title="" />
         );
       })}
       {!photos && <div>Loading...</div>}
