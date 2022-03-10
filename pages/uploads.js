17bd764287a985e57af57b15173140ae10f4acc0
Ibarra <aitoribarra@tutanota.com>
2022-03-10 11:35:39 +0100

    REFACTOR - Uploads (Clean up)

--- pages/uploads.js
+++ pages/uploads.js
@@ -1,11 +1,6 @@
-import React, { useRef } from "react";
+import React from "react";
 
 import Input from "../components/Input/Input";
-
-const postImage = fetch("http://localhost:4000/api/v1/upload", {
-  method: "POST",
-  body: "",
-});
 
 const uploads = () => {
   return (
