7507ae552380c6b1d05fc81d4f6fe9eccc02de11
Ibarra <aitoribarra@tutanota.com>
2022-04-15 21:29:40 +0200

    ADD - Log out handler

--- pages/uploads.js
+++ pages/uploads.js
@@ -3,9 +3,16 @@ import React from "react";
 import Input from "../components/Input/Input";
 import Login from "../components/Login/Login";
 
+import style from "../styles/uploads.module.css";
+
 const uploads = () => {
+  const logoutHandler = () => {};
+
   return (
-    <div>
+    <div className={style.uploads}>
+      <button className={style.logout} onClick={logoutHandler}>
+        Logout
+      </button>
       <Input />
       <Login />
     </div>
