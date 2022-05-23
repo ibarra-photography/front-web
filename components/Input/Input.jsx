45bb17a05f4c8ec7a653ec1e41d5234b1c55794c
Ibarra <aitoribarra@tutanota.com>
2022-05-23 22:01:59 +0200

    UPDATE - Input file component

--- components/Input/Input.jsx
+++ components/Input/Input.jsx
@@ -1,44 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import axios from "axios";
+import uploadPhoto from "../../services/uploadPhotos";
+
+import LoginContext from "../../store/login-context";
 
 import styles from "./input.module.css";
+import authenticate from "../../services/authenticate";
 
 const Input = () => {
-  const [uploadedImage, setUploadedImage] = useState("");
+  const [uploadedImage, setUploadedImage] = useState();
+  const [text, setText] = useState();
+  const [title, setTitle] = useState();
+
+  const loginCtx = useContext(LoginContext);
+
+  const { credentials, logOut } = loginCtx;
+
+  console.log("Cred: ", credentials);
 
   const handleInput = (event) => {
     console.log(event.target.files[0]);
 
     setUploadedImage(event.target.files[0]);
   };
+
   const formData = () => {
     if (uploadedImage) {
       const formData = new FormData();
-      uploadedImage instanceof File;
-      uploadedImage instanceof Blob;
-      formData.append("MyPhoto.jpg", uploadedImage);
-      console.log("Form Data", formData);
+      formData.append("photo", uploadedImage);
+      formData.append("text", text);
+      formData.append("title", title);
+      formData.append("token", credentials.token);
+
       return formData;
     }
   };
 
+  const handleTextInput = (event) => {
+    event.preventDefault();
+    setText(event.target.value);
+  };
+
+  const handleTitleInput = (event) => {
+    event.preventDefault();
+    setTitle(event.target.value);
+  };
+
   const postImage = async () => {
-    const res = await axios.post(
-      "http://localhost:4000/api/v1/upload",
-      formData(),
-      {
-        headers: {
-          "Content-Type": "multipart/format-data",
-        },
+    const photo = formData();
+    try {
+      const res = await uploadPhoto(photo);
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 401) {
+        logOut();
       }
-    );
-    console.log(res);
+    }
   };
 
   return (
     <div className={styles.input}>
       <h2>Uploads</h2>
+      <h3>Title</h3>
+      <input type="text " name="Title" onChange={handleTitleInput} />
+      <h3>Text</h3>
+      <input type="text" onChange={handleTextInput} value={text} />
+      <h3>File</h3>
       <input type="file" name="image" onChange={handleInput} />
       <button onClick={() => postImage()} className={styles.submit}>
         Submit
