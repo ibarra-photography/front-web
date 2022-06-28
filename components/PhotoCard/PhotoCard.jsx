2369e390859b3172106b7d835e8da8e6844f9861
Ibarra <aitoribarra@tutanota.com>
2022-06-28 23:42:43 +0200

    UPDATE - Image

--- components/PhotoCard/PhotoCard.jsx
+++ components/PhotoCard/PhotoCard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+import Image from "next/image";
+
 import styles from "./photo-card.module.css";
 
 const PhotoCard = ({ link, title, description }) => {
   return (
     <div>
-      <img className={styles.image} src={link} alt={description} />
+      <Image
+        src={link}
+        alt={description}
+        layout="intrinsic"
+        width={200}
+        height={200}
+      />
       <div className={styles.title}>{title}</div>
       <div className={styles.description}>{description}</div>
     </div>
