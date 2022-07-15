2ca3080d3d8d15cf313d267447089ed392527e05
Aitor <ibarraaitor@pm.me>
2022-07-15 23:42:19 +0200

    ADD - Env var for prod and deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "custom",
   },
   env: {
-    API_URL: "http://localhost:4000",
+    API_URL: PHASE_DEVELOPMENT_SERVER
+      ? "http://localhost:4000"
+      : "https://ibarra-photography.herokuapp.com/",
   },
 };
 
--- services/fetchApiData.js
+++ services/fetchApiData.js
@@ -1,6 +1,5 @@
 const fetchApiData = async () => {
   const endpoint = `${process.env.API_URL}/api/v1/photos`;
-  console.log(process.env.API_URL);
 
   const response = await fetch(endpoint);
   const data = await response.json();
