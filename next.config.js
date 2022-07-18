cec3106e6a7a1b151eb3e0cf9ed8e9c63bdc8ee2
Aitor <ibarraaitor@pm.me>
2022-07-18 23:30:40 +0200

    UPDATE - Env Var

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ const nextConfig = {
     loader: "custom",
   },
   env: {
-    API_URL: PHASE_DEVELOPMENT_SERVER
-      ? "http://localhost:4000"
-      : "https://ibarra-photography.herokuapp.com/",
+    API_URL: PHASE_DEVELOPMENT_SERVER && "http://localhost:4000",
+
+    // "https://ibarra-photography.herokuapp.com/",
   },
 };
 
