db610de8508fd9f1bb5734eb48f40acd5ae9ff97
Aitor <ibarraaitor@pm.me>
2022-07-19 14:03:40 +0200

    UPDATE - Config Next

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
 /** @type {import('next').NextConfig} */
-const { PHASE_BUILD_SERVER } = require("next/constants");
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-const nextConfig = {
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      images: {
+        loader: "custom",
+      },
+      env: {
+        API_URL: "http://localhost:4000",
+
+        // "https://ibarra-photography.herokuapp.com/",
+      },
+    };
+  }
+};
+return {
   reactStrictMode: true,
   images: {
     loader: "custom",
   },
   env: {
-    API_URL: PHASE_BUILD_SERVER
-      ? "https://ibarra-photography.herokuapp.com/"
-      : "http://localhost:4000",
+    API_URL: "https://ibarra-photography.herokuapp.com/",
+    //  "http://localhost:4000",
 
     // "https://ibarra-photography.herokuapp.com/",
   },
