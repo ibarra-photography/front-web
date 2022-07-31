4b1b6c182f02f819191bec3662eb0f17906adfd4
Aitor <ibarraaitor@pm.me>
2022-07-31 13:29:27 +0200

    REFACTOR - Spinner

--- components/Spinner/Spinner.jsx
+++ components/Spinner/Spinner.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-
 import styles from "./spinner.module.css";
 
-const Spinner = ({ containerStyles }) => {
-  return (
-    <div className={styles["loader-container"]} style={containerStyles}>
-      <span className={styles.loader}></span>
-    </div>
-  );
-};
+const Spinner = ({ containerStyles }) => (
+  <div className={styles["loader-container"]} style={containerStyles}>
+    <span className={styles.loader} />
+  </div>
+);
 
 export default Spinner;
