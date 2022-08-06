c6fa939cb3bcc9a55eff5611b258a892d44097df
Aitor <ibarraaitor@pm.me>
2022-08-06 20:42:41 +0200

    TEST - Mock post image

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,15 +1,22 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.post("/uploads", (req, res, ctx) => {
-    // Persist user's authentication in the session
+  rest.post(
+    "/uploads",
+    (req, res, ctx) => {
+      // Persist user's authentication in the session
 
-    sessionStorage.setItem("is-authenticated", "true");
+      sessionStorage.setItem("is-authenticated", "true");
 
-    return res(
-      // Respond with a 200 status code
+      return res(
+        // Respond with a 200 status code
 
-      ctx.status(200)
-    );
-  }),
+        ctx.status(200)
+      );
+    },
+    rest.post("http://localhost:4000/api/v1/upload", (req, res, ctx) => {
+      console.log("Mocking API uploads");
+      return res(ctx.status(200));
+    })
+  ),
 ];
