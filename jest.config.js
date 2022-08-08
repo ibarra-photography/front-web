b1fffe758d815bb7d6b38324cbbe33a7c72e2f31
Aitor <ibarraaitor@pm.me>
2022-08-09 00:00:21 +0200

    TEST - jest config

--- jest.config.js
+++ jest.config.js
@@ -13,8 +13,10 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
+  setupFilesAfterEnv: ["<rootDir>test/setupTest.js"],
   testEnvironment: "jest-environment-jsdom",
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "jsx"],
+  clearMocks: true,
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "jsx", "js"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
