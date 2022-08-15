0cb96094f44683bf1dd36813257b6771a1797018
Aitor <ibarraaitor@pm.me>
2022-08-15 20:08:02 +0200

    ADD - Redirect on image loading

--- components/Hooks/usePostImage.jsx
+++ components/Hooks/usePostImage.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import { useRouter } from "node_modules/next/router";
 
 import LoginContext from "store/login-context";
 
@@ -7,6 +8,8 @@ import uploadPhoto from "services/uploadPhotos";
 const usePostImage = () => {
   const { credentials, logOut } = useContext(LoginContext);
   const [loadingStatus, setLoadingStatus] = useState("idle");
+
+  const router = useRouter();
 
   const uploadImage = async (photo, title, text) => {
     if (loadingStatus === "loading") return;
@@ -23,6 +26,7 @@ const usePostImage = () => {
       const res = await uploadPhoto(formData);
       if (res.status === 200) {
         setLoadingStatus("success");
+        router.push("/");
       }
     } catch (error) {
       setLoadingStatus("error");
