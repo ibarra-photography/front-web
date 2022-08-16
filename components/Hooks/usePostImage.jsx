10c13c6051cb70a661defc8d90c01a6e45c4c2dd
Aitor <ibarraaitor@pm.me>
2022-08-16 21:26:24 +0200

    DEL - Redirect

--- components/Hooks/usePostImage.jsx
+++ components/Hooks/usePostImage.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from "react";
-import { useRouter } from "node_modules/next/router";
 
 import LoginContext from "store/login-context";
 
@@ -8,8 +7,6 @@ import uploadPhoto from "services/uploadPhotos";
 const usePostImage = () => {
   const { credentials, logOut } = useContext(LoginContext);
   const [loadingStatus, setLoadingStatus] = useState("idle");
-
-  const router = useRouter();
 
   const uploadImage = async (photo, title, text) => {
     if (loadingStatus === "loading") return;
@@ -26,7 +23,6 @@ const usePostImage = () => {
       const res = await uploadPhoto(formData);
       if (res.status === 200) {
         setLoadingStatus("success");
-        router.push("/");
       }
     } catch (error) {
       setLoadingStatus("error");
