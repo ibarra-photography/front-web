4bd680e793c2efac7b7d6d37d48b9021f1dbd488
Aitor <ibarraaitor@pm.me>
2022-08-21 16:46:34 +0200

    ADD - Responsive photo gallery card grid

--- components/PhotoCardsGrid/PhotoCardsGrid.jsx
+++ components/PhotoCardsGrid/PhotoCardsGrid.jsx
@@ -9,6 +9,8 @@ import b64toBlob from "../../services/base64toBlob.js";
 import Skeleton from "components/Skeleton/Skeleton";
 import Pagination from "components/Pagination/Pagination";
 
+import styles from "./photo-cards-grid.module.css";
+
 const PhotoCardsGrid = () => {
   const [isLoading, setIsLoading] = useState("idle");
   const [photos, setPhotos] = useState([]);
@@ -32,37 +34,39 @@ const PhotoCardsGrid = () => {
   }, [getPhotosHandler]);
 
   return (
-    <Fragment>
-      {isLoading === "done"
-        ? photos.map((photo) => {
-            const blob = b64toBlob(photo.photo);
-            const blobUrl = URL.createObjectURL(blob);
-            return (
-              <Button key={photo._id} path={`/gallery/${photo._id}`}>
-                <PhotoCard
-                  key={photo._id}
-                  description=""
-                  link={blobUrl}
-                  title=""
-                />
-              </Button>
-            );
-          })
-        : null}
-      {isLoading === "pending" ? (
-        <Fragment>
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-        </Fragment>
-      ) : null}
+    <div className={styles["photo-card-grid-container"]}>
+      <div className={styles["photo-card-grid-photo-container"]}>
+        {isLoading === "done"
+          ? photos.map((photo) => {
+              const blob = b64toBlob(photo.photo);
+              const blobUrl = URL.createObjectURL(blob);
+              return (
+                <Button key={photo._id} path={`/gallery/${photo._id}`}>
+                  <PhotoCard
+                    key={photo._id}
+                    description=""
+                    link={blobUrl}
+                    title=""
+                  />
+                </Button>
+              );
+            })
+          : null}
+        {isLoading === "pending" ? (
+          <Fragment>
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+          </Fragment>
+        ) : null}
+      </div>
       <Pagination pageHandler={getPhotosHandler} />
-    </Fragment>
+    </div>
   );
 };
 
