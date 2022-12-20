9b914e1b904898836c9dac5f0c6d369e8bf07def
Aitor <ibarraaitor@pm.me>
2022-12-20 21:03:09 +0100

    ADD - Grid display on photos

--- src/components/PhotoGrid/PhotoGrid.tsx
+++ src/components/PhotoGrid/PhotoGrid.tsx
@@ -2,10 +2,16 @@ import PhotoCard from 'components/PhotoCard';
 
 import { ApiPhoto } from 'domain/Models/Photo/ApiPhoto';
 
+import { PhotoGridStyles } from './PhotoGridStyles';
+
 interface Props {
   photos: Array<ApiPhoto>;
 }
 
 export const PhotoGrid = ({ photos }: Props) => {
-  return <div>{photos && photos.map(({ _id, photo, text, title }) => <PhotoCard key={_id} description={text} link={photo} title={title} />)}</div>;
+  return (
+    <PhotoGridStyles.Container>
+      {photos && photos.map(({ _id, photo, text, title }) => <PhotoCard key={_id} description={text} link={photo} title={title} />)}
+    </PhotoGridStyles.Container>
+  );
 };
