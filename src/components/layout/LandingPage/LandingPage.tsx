77f912fa3ccb7aa9bd58cc29301844a06e9d0ce5
Aitor <ibarraaitor@pm.me>
2023-03-05 21:14:26 +0100

    UPDATE - Landing page

--- src/components/layout/LandingPage/LandingPage.tsx
+++ src/components/layout/LandingPage/LandingPage.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
-import Image, { ImageLoader } from 'next/image';
+import Image from 'next/image';
 
 import { LandingPageStyles } from './LandingPage.styles';
 
 import landingPageImg from 'domain/media/img/landing-page.jpg';
+import LandingPageInformation from 'components/LandingPageInformation';
 
 export const LandingPage = () => {
-  const myLoader: ImageLoader = ({ src, width, quality }) => {
-    return `${src}?w=${width}&q=${quality || 100}`;
-  };
-
   return (
     <LandingPageStyles.LandingPageWrapper>
       <LandingPageStyles.ImageContainer>
         <Image src={landingPageImg} fill className="landing-image" alt={''} />
       </LandingPageStyles.ImageContainer>
-      <LandingPageStyles.InformationContainer>Information</LandingPageStyles.InformationContainer>
+      <LandingPageStyles.InformationContainer>
+        <LandingPageInformation />
+      </LandingPageStyles.InformationContainer>
     </LandingPageStyles.LandingPageWrapper>
   );
 };
