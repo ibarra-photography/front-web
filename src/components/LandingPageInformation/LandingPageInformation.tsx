369c6d1220fc4a285cb97370d65fa08114026b59
Aitor <ibarraaitor@pm.me>
2023-03-06 19:57:54 +0100

    REFACTOR - landing page module css

--- src/components/LandingPageInformation/LandingPageInformation.tsx
+++ src/components/LandingPageInformation/LandingPageInformation.tsx
@@ -4,38 +4,39 @@ import Link from 'next/link';
 
 import Navigation from 'components/Navigation';
 
-import { LandingPageInformationStyles } from './LandingPageInformation.styles';
 import SocialMedia from 'components/SocialMedia';
+
+import LandingPageInformationStyles from './LandingPage.module.css';
 
 export function LandingPageInformation() {
   return (
-    <LandingPageInformationStyles.InformationContainer>
-      <LandingPageInformationStyles.NavigationContainer>
+    <div className={LandingPageInformationStyles.informationContainer}>
+      <nav className={LandingPageInformationStyles.navigationContainer}>
         <Navigation />
-      </LandingPageInformationStyles.NavigationContainer>
-      <LandingPageInformationStyles.Information>
+      </nav>
+      <p className={LandingPageInformationStyles.information}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero soluta ex ea et pariatur a obcaecati quis, dolores maxime. Nostrum perferendis
         reiciendis temporibus aspernatur et culpa beatae maiores nisi id! Labore eius, odio temporibus nesciunt ab maiores veniam ipsam odit
         laudantium ratione nihil. Vel dolorum doloremque, quam cum, eum cupiditate reiciendis maiores accusamus sunt, perferendis dolores aliquid
         laboriosam minus tempora? Rerum vero aperiam corrupti tempora provident quia et ratione officia voluptate sequi. Ad dignissimos ipsum id illum
         cumque alias fugiat delectus error maiores nostrum? Exercitationem deserunt veniam repudiandae fuga molestiae.
-      </LandingPageInformationStyles.Information>
-      <LandingPageInformationStyles.Information>
+      </p>
+      <p className={LandingPageInformationStyles.information}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus ipsam ab laborum sint explicabo neque architecto tempore quaerat soluta
         distinctio? Repellendus, porro quod magnam perspiciatis possimus dolor soluta aut ad. Quos, voluptates perferendis iste commodi harum id amet
         esse delectus nemo dolor repellat quod totam dolorem, cupiditate molestiae est eaque ut accusamus a reiciendis! Laudantium totam magnam
         corrupti delectus in!
-      </LandingPageInformationStyles.Information>
-      <LandingPageInformationStyles.News>
+      </p>
+      <div className={LandingPageInformationStyles.news}>
         Prints available soon!
-        <Link href={'notify-me/prints'} className="notify-me">
+        <Link href={'notify-me/prints'} className={LandingPageInformationStyles.notifyMe}>
           Notify me
         </Link>
-      </LandingPageInformationStyles.News>
-      <LandingPageInformationStyles.SocialMediaContainer>
+      </div>
+      <p className={LandingPageInformationStyles.socialMedia}>
         <SocialMedia />
-      </LandingPageInformationStyles.SocialMediaContainer>
-      <LandingPageInformationStyles.Rights>© ibarra_photography 2023</LandingPageInformationStyles.Rights>
-    </LandingPageInformationStyles.InformationContainer>
+      </p>
+      <p className={LandingPageInformationStyles.rights}>© ibarra_photography 2023</p>
+    </div>
   );
 }
