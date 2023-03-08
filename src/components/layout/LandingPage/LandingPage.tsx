e4a31400d0fe7e444c01d3084e06c813be5c8495
Aitor <ibarraaitor@pm.me>
2023-03-08 22:31:45 +0100

    UPDATE - Title styles

--- src/components/layout/LandingPage/LandingPage.tsx
+++ src/components/layout/LandingPage/LandingPage.tsx
@@ -11,6 +11,7 @@ export const LandingPage = () => {
   return (
     <div className={LandingPageStyles.landingPageWrapper}>
       <div className={LandingPageStyles.imageContainer}>
+        <h1 className={LandingPageStyles.title}>Ibarra Photography</h1>
         <Image src={landingPageImg} fill className={LandingPageStyles.landingImage} alt={''} />
       </div>
       <div className={LandingPageStyles.informationContainer}>
