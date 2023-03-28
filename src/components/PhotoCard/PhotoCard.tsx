056982417c0ae802ac52cddcd7dfb80579149905
Aitor <ibarraaitor@pm.me>
2023-03-28 20:57:36 +0200

    UPDATE - Photo Card no blob conversions

--- src/components/PhotoCard/PhotoCard.tsx
+++ src/components/PhotoCard/PhotoCard.tsx
@@ -5,22 +5,25 @@ import Image, { ImageLoader } from 'next/image';
 import { b64toBlob } from 'application/utils/base64toBlob';
 
 import { PhotoCardStyles } from './PhotoCard.styles';
+import './PhotoCard.css';
 interface Props {
-  link: string;
+  link: Blob;
   title: string;
   description: string;
 }
 
 export const PhotoCard = ({ link, title, description }: Props) => {
-  const blob = b64toBlob(link);
-  const src = window.URL.createObjectURL(blob);
+  // const blob = b64toBlob(link);
+  const src = window.URL.createObjectURL(link);
+
+  // const src = '';
 
   const myLoader: ImageLoader = ({ src, width, quality }) => {
     return `${src}?w=${width}&q=${quality || 100}`;
   };
   return (
-    <PhotoCardStyles.PhotoCardContainer aria-label="image">
+    <div aria-label="image" className="container">
       <Image className="image" loader={myLoader} src={src} alt={description} width={200} height={200} />
-    </PhotoCardStyles.PhotoCardContainer>
+    </div>
   );
 };
