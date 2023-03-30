db69882a26bf3954f1a081436e1c93d6a50a19b2
Aitor <ibarraaitor@pm.me>
2023-03-30 20:34:57 +0200

    UPDATE - Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import { IFetchParams, useFetch } from 'hooks/useFetch';
+import { redirect } from 'next/navigation';
+import React, { useEffect } from 'react';
 
-export const Dashboard = () => {
-  return <div>Dashboard</div>;
+interface IProps {
+  user: string;
+}
+interface IResponse {
+  user: string;
+  isValid: boolean;
+}
+
+export const Dashboard = ({ user }: IProps) => {
+  const { fetcher, fetchingStatus, response } = useFetch<IResponse>();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    const body = { user, token };
+    const validateSettings: IFetchParams = {
+      method: 'POST',
+      url: `${process.env.API_URL}/api/v1/validate/dashboard`,
+      body,
+      headers: { 'Content-Type': 'application/json' }
+    };
+    fetcher(validateSettings);
+  }, []);
+
+  if (fetchingStatus === 'succeeded' && !response?.isValid) redirect('/login');
+  console.log('fetchingStatus', fetchingStatus);
+
+  return (
+    <div>
+      <h1>{user}</h1>
+      <div>
+        {fetchingStatus === 'loading' ? <p>loading...</p> : null}
+        {fetchingStatus === 'succeeded' ? <p>Dashboard: {response?.user}</p> : null}
+      </div>
+    </div>
+  );
 };
