4acad4276f43a878fc3707d4fee438e9ec4b8f80
Aitor <ibarraaitor@pm.me>
2023-04-10 17:09:59 +0200

    UPDATE - Gallery styles

--- src/components/layout/GalleryPage/GalleryPage.tsx
+++ src/components/layout/GalleryPage/GalleryPage.tsx
@@ -7,12 +7,16 @@ import { ApiPhoto } from 'domain/Models/Photo/ApiPhoto';
 import { fetchPhotos } from 'service/fetchPhotos';
 
 import GalleryStyles from './GalleryPage.module.css';
+import Navigation from 'components/Navigation';
 
 export const GalleryPage = () => {
   const photos: ApiPhoto[] = use(fetchPhotos(1)).data;
   return (
-    <div>
-      <h2>Gallery</h2>
+    <div className={GalleryStyles.page}>
+      <div className={GalleryStyles.header}>
+        <h2 className={GalleryStyles.title}>Gallery</h2>
+        <Navigation />
+      </div>
       <div className={GalleryStyles.container}>
         {photos ? photos.map(({ _id, photo, text, title }) => <PhotoCard key={_id} description={text} link={photo} title={title} />) : null}
       </div>
