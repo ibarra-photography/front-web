9d29993e75a290b03676089c5b5f6e1c5a8421e4
Aitor <ibarraaitor@pm.me>
2023-04-25 20:37:10 +0200

    ADD - Link photo card component

--- src/components/PhotoCard/PhotoCard.tsx
+++ src/components/PhotoCard/PhotoCard.tsx
@@ -6,22 +6,25 @@ import { b64toBlob } from 'application/utils/base64toBlob';
 
 import { PhotoCardStyles } from './PhotoCard.styles';
 import './PhotoCard.css';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
 interface Props {
   link: Blob;
   title: string;
   description: string;
+  id: string;
 }
 
-export const PhotoCard = ({ link, title, description }: Props) => {
+export const PhotoCard = ({ link, title, description, id }: Props) => {
   const srcBs64 = `data:image/jpeg;base64,${link}`;
 
   const myLoader: ImageLoader = ({ src, width, quality }) => {
     return `${src}?w=${width}&q=${quality || 100}`;
   };
+
   return (
-    <div aria-label="image" className="container">
-      {/* <Image className="image" loader={myLoader} src={srcBs64} alt={description} width={200} height={200} /> */}
+    <Link href={`gallery/photo/${id}`} aria-label="image" className="container">
       <img className="image" src={srcBs64} />
-    </div>
+    </Link>
   );
 };
