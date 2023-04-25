49ab5ff481c5eb152b22a520e80e4b449535d6ff
Aitor <ibarraaitor@pm.me>
2023-04-25 20:32:27 +0200

    UPDATE - Photo uploading system

--- src/components/Dashboard/UploadImage/UploadImage.tsx
+++ src/components/Dashboard/UploadImage/UploadImage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { use, useRef, useState, type FormEvent } from 'react';
 
 import { IFetchParams, useFetch } from 'hooks/useFetch';
 
@@ -9,8 +9,8 @@ interface IProps {
 }
 
 export const UploadImage = ({ user }: IProps) => {
-  const { fetcher, fetchingStatus, response } = useFetch();
   const [photoFile, setPhotoFile] = useState<FileList>();
+  const [postingState, setPostingState] = useState<'idle' | 'loading' | 'success' | 'error'>('idle');
 
   const photoFileRef = useRef<HTMLInputElement>(null);
   const photoNameRef = useRef<HTMLInputElement>(null);
@@ -21,65 +21,56 @@ export const UploadImage = ({ user }: IProps) => {
     setPhotoFile(event.target.files!);
   };
 
-  const uploadHandler = async () => {
-    debugger;
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    if (postingState === 'loading') return;
+    event.preventDefault();
+    setPostingState('loading');
 
     const token = window.localStorage.getItem('token') || '';
     if (photoFile) {
-      console.log('photoFile', photoFile[0]);
-
       const text = photoDescriptionRef.current?.value || '';
       const title = photoNameRef.current?.value || '';
 
-      const formData = new FormData();
+      const formData = new FormData(event.currentTarget);
       formData.append('photo', photoFile[0]);
       formData.append('text', text);
       formData.append('title', title);
       formData.append('token', token);
       formData.append('username', user);
 
-      const fetcherSettings: IFetchParams = {
-        url: `${process.env.API_URL}/api/v1/upload`,
-        method: 'POST',
-        body: formData,
-        headers: { 'Content-Type': 'multipart/form-data' }
-      };
-
-      //fetcher(fetcherSettings);
-      const res = await fetch(`${process.env.API_URL}/api/v1/upload`, { method: 'POST', body: formData });
+      try {
+        const res = await fetch(`${process.env.API_URL}/api/v1/upload`, { method: 'POST', body: formData });
+        setPostingState('success');
+        formRef.current?.reset();
+      } catch (error) {
+        setPostingState('error');
+      }
     } else {
       console.log('There is no photo');
+      setPostingState('idle');
     }
-  };
-
-  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    uploadHandler();
   };
 
   return (
     <div className={UploadImageStyles.container}>
       <h3>Upload image</h3>
-      {fetchingStatus !== 'loading' ? (
-        <form action="" onSubmit={submitHandler} className={UploadImageStyles.form} ref={formRef}>
-          <span className={UploadImageStyles.inputContainer}>
-            <label>Photo name</label>
-            <input type="text" alt="photo-name" ref={photoNameRef} />
-          </span>
-          <span className={UploadImageStyles.inputContainer}>
-            <label>Description</label>
-            <input type="text" alt="photo-description" ref={photoDescriptionRef} />
-          </span>
-          <span className={UploadImageStyles.inputContainer}>
-            <label>Photo</label>
-            <input type="file" alt="photo-file" ref={photoFileRef} onChange={photoInputHandler} />
-          </span>
-          <span className={UploadImageStyles.buttonContainer}>
-            <button onChange={uploadHandler}>Upload photo</button>
-          </span>
-        </form>
-      ) : null}
-      {fetchingStatus === 'loading' ? <p>Loading ...</p> : null}
+      <form action="" onSubmit={submitHandler} className={UploadImageStyles.form} ref={formRef}>
+        <span className={UploadImageStyles.inputContainer}>
+          <label>Photo name</label>
+          <input type="text" alt="photo-name" ref={photoNameRef} />
+        </span>
+        <span className={UploadImageStyles.inputContainer}>
+          <label>Description</label>
+          <input type="text" alt="photo-description" ref={photoDescriptionRef} />
+        </span>
+        <span className={UploadImageStyles.inputContainer}>
+          <label>Photo</label>
+          <input type="file" alt="photo-file" ref={photoFileRef} onChange={photoInputHandler} />
+        </span>
+        <span className={UploadImageStyles.buttonContainer}>
+          {postingState === 'loading' ? <p>Loading...</p> : <button type="submit">Upload photo</button>}
+        </span>
+      </form>
     </div>
   );
 };
