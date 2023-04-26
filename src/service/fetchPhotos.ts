313b8aa861c0f337f685b12d341a54e78373937e
Aitor <ibarraaitor@pm.me>
2023-04-26 07:28:58 +0200

    UPDATE - fetch photo

--- src/service/fetchPhotos.ts
+++ src/service/fetchPhotos.ts
@@ -4,5 +4,5 @@ export const fetchPhotos = cache((page: number) => {
   const endpoint = new URL(`${process.env.API_URL}/api/v1/photos`);
   endpoint.searchParams.append('page', String(page));
 
-  return fetch(`http://localhost:4000/api/v1/photos?page=${page}`).then(res => res.json());
+  return fetch(endpoint).then(res => res.json());
 });
