4a97efc6645e8387a68c8a0de88438065110f496
Aitor <ibarraaitor@pm.me>
2023-04-27 21:04:57 +0200

    ADD - home page texts

--- src/components/LandingPageInformation/LandingPageInformation.tsx
+++ src/components/LandingPageInformation/LandingPageInformation.tsx
@@ -8,25 +8,20 @@ import SocialMedia from 'components/SocialMedia';
 
 import LandingPageInformationStyles from './LandingPage.module.css';
 
+import homePageTexts from 'domain/texts/homePage/homePage.json';
+
 export function LandingPageInformation() {
   return (
     <div className={LandingPageInformationStyles.informationContainer}>
       <nav className={LandingPageInformationStyles.navigationContainer}>
         <Navigation />
       </nav>
-      <p className={LandingPageInformationStyles.information}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero soluta ex ea et pariatur a obcaecati quis, dolores maxime. Nostrum perferendis
-        reiciendis temporibus aspernatur et culpa beatae maiores nisi id! Labore eius, odio temporibus nesciunt ab maiores veniam ipsam odit
-        laudantium ratione nihil. Vel dolorum doloremque, quam cum, eum cupiditate reiciendis maiores accusamus sunt, perferendis dolores aliquid
-        laboriosam minus tempora? Rerum vero aperiam corrupti tempora provident quia et ratione officia voluptate sequi. Ad dignissimos ipsum id illum
-        cumque alias fugiat delectus error maiores nostrum? Exercitationem deserunt veniam repudiandae fuga molestiae.
-      </p>
-      <p className={LandingPageInformationStyles.information}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus ipsam ab laborum sint explicabo neque architecto tempore quaerat soluta
-        distinctio? Repellendus, porro quod magnam perspiciatis possimus dolor soluta aut ad. Quos, voluptates perferendis iste commodi harum id amet
-        esse delectus nemo dolor repellat quod totam dolorem, cupiditate molestiae est eaque ut accusamus a reiciendis! Laudantium totam magnam
-        corrupti delectus in!
-      </p>
+      <div>
+        {homePageTexts.description.map(description => (
+          <p className={LandingPageInformationStyles.information}>{description}</p>
+        ))}
+      </div>
+
       <h3 className={LandingPageInformationStyles.news}>
         Prints available soon!
         <Link href={'notify-me/prints'} className={LandingPageInformationStyles.notifyMe}>
