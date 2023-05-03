363a34abc9eb41ae5a1f52dc88b0c6a9da641f26
Aitor <ibarraaitor@pm.me>
2023-05-03 20:29:04 +0200

    ADD - Social media links

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 import { FaInstagram, FaTwitter, FaTelegram } from 'react-icons/fa';
@@ -8,13 +9,19 @@ export const SocialMedia = () => {
   return (
     <div className={SocialMediaStyles.socialMediaContainer}>
       <div>
-        <FaInstagram />
+        <Link href={'https://www.instagram.com/ibarra__photography/'} target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </Link>
       </div>
       <div>
-        <FaTwitter />
+        <Link href={'https://twitter.com/ibarra_photo'} target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </Link>
       </div>
       <div>
-        <FaTelegram />
+        <Link href={'/'}>
+          <FaTelegram />
+        </Link>
       </div>
     </div>
   );
