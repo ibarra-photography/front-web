585f60693c80304b28c049a9fada26ade236d75a
Aitor <ibarraaitor@pm.me>
2023-05-03 06:17:40 +0200

    ADD - Gallery error styles

--- src/app/gallery/error.tsx
+++ src/app/gallery/error.tsx
@@ -1,16 +1,22 @@
 'use client';
+import Navigation from 'components/Navigation';
+import Link from 'next/link';
 
-import { useEffect } from 'react';
+import ErrorStyles from './error.module.css';
 
-export default function ErrorFallback({ error, reset }: { error: Error; reset: () => void }) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
+export default function GalleryErrorPage() {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button onClick={() => reset()}>Try again</button>
+    <div className={ErrorStyles.page}>
+      <div className={ErrorStyles.header}>
+        <h2 className={ErrorStyles.title}>Gallery</h2>
+        <Navigation />
+      </div>
+      <div className={ErrorStyles.container}>
+        <h2>Some thing went wrong on loading the gallery</h2>
+        <p>Try reloading it ot visit the page later</p>
+        <p>If the error persists contact to incidents@ibarraphoto.me</p>
+        <Link href={'/gallery'}>Try to reload</Link>
+      </div>
     </div>
   );
 }
