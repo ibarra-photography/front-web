3bfc6fb0b2c548cfcf0b91848dea06ee75685398
Aitor <ibarraaitor@pm.me>
2023-05-03 20:49:54 +0200

    ADD - Media query

--- src/components/layout/LandingPage/LandingPage.tsx
+++ src/components/layout/LandingPage/LandingPage.tsx
@@ -10,8 +10,8 @@ import LandingPageInformation from 'components/LandingPageInformation';
 export const LandingPage = () => {
   return (
     <div className={LandingPageStyles.landingPageWrapper}>
+      <h1 className={LandingPageStyles.title}>Ibarra Photography</h1>
       <div className={LandingPageStyles.imageContainer}>
-        <h1 className={LandingPageStyles.title}>Ibarra Photography</h1>
         <Image src={landingPageImg} fill className={LandingPageStyles.landingImage} alt={''} />
       </div>
       <div className={LandingPageStyles.informationContainer}>
