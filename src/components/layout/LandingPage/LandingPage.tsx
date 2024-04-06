e8c3653712f6c243f1783cc4f1478039f70517ac
Aitor <ibarraaitor@pm.me>
2024-04-06 16:11:11 +0200

    UPDATE - Landing styles

--- src/components/layout/LandingPage/LandingPage.tsx
+++ src/components/layout/LandingPage/LandingPage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Image from 'next/image';
 
+import Navigation from 'components/Navigation';
+
 import LandingPageStyles from './LandingPage.module.css';
 
 import landingPageImg from '../../../../public/media/img/landing-page.jpg';
@@ -9,14 +11,22 @@ import LandingPageInformation from 'components/LandingPageInformation';
 
 export const LandingPage = () => {
   return (
-    <div className={LandingPageStyles.landingPageWrapper}>
-      <h1 className={LandingPageStyles.title}>Ibarra Photography</h1>
-      <div className={LandingPageStyles.imageContainer}>
-        <Image src={landingPageImg} fill className={LandingPageStyles.landingImage} alt={''} />
-      </div>
-      <div className={LandingPageStyles.informationContainer}>
-        <LandingPageInformation />
-      </div>
-    </div>
+    <main className={LandingPageStyles.landingPageWrapper}>
+      <section className={LandingPageStyles.heroSection}>
+        <Image src={landingPageImg} height="500" width="1000" className={LandingPageStyles.landingImage} alt={'Image of a vulture in the sky'} />
+        <div className={LandingPageStyles.titleContainer}>
+          <h1 className={LandingPageStyles.title}>Ibarra Photography</h1>
+          <nav className={LandingPageStyles.navigation}>
+            <Navigation page="home" />
+          </nav>
+        </div>
+      </section>
+
+      <section>
+        <div className={LandingPageStyles.informationContainer}>
+          <LandingPageInformation />
+        </div>
+      </section>
+    </main>
   );
 };
