b531f198530e9197ffff937a99bc5cdf9ebcde76
Aitor <ibarraaitor@pm.me>
2024-06-21 04:16:22 +0200

    ADD - Static image pages

--- src/app/gallery/photo/[photoId]/page.tsx
+++ src/app/gallery/photo/[photoId]/page.tsx
@@ -1,9 +1,18 @@
-import { Suspense } from 'react';
+import { Suspense, use } from 'react';
 
 import { PhotoPage } from 'components/layout/PhotoPage/PhotoPage';
 
 import SuspenseFallback from 'components/SuspenseFallback';
 import PhotoPageSkeleton from 'components/layout/PhotoPage/PhotoPageSkeleton';
+import { ApiPhoto } from 'domain/Models/Photo/ApiPhoto';
+import { fetchPhotos } from 'service/fetchPhotos';
+
+export async function generateStaticParams() {
+  const res = await fetchPhotos(1);
+  const photos: ApiPhoto[] = res.data;
+
+  return photos.map(photo => photo._id);
+}
 
 interface IParams {
   params: { photoId: string };
